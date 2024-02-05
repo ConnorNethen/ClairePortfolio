c63ec6c60ff1133b1c6e8d0f139e1c3a52ecf986
ConnorNethen <ConnorNethen258@gmail.com>
2024-02-05 17:58:06 -0600

    Added swipe functionality and edited styles for smaller devices

--- static/javascript/carousel.js
+++ static/javascript/carousel.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentSlide = 0;
     const carousel = document.getElementById('carousel');
     const totalSlides = 3;
+    let touchStartX = 0;
+    let touchEndX = 0;
     
     document.getElementById('nextSlide').addEventListener('click', function() {
       nextSlide();
@@ -10,6 +12,15 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('prevSlide').addEventListener('click', function() {
       prevSlide();
     });
+
+    carousel.addEventListener('touchstart', function(event) {
+      touchStartX = event.touches[0].clientX;
+    }, false);
+
+    carousel.addEventListener('touchend', function(event) {
+      touchEndX = event.changedTouches[0].clientX;
+      handleSwipe();
+    }, false);
   
     function nextSlide() {
       currentSlide = (currentSlide + 1) % totalSlides;
@@ -25,5 +36,18 @@ document.addEventListener('DOMContentLoaded', function () {
       const translateX = currentSlide * -100;
       carousel.style.transform = `translateX(${translateX}%)`;
     }
-  });
+
+    function handleSwipe() {
+      const threshold = 70;
+      let distance = touchEndX - touchStartX;
+
+      if (Math.abs(distance) >= threshold) {
+        if (distance < 0) {
+          nextSlide();
+        } else {
+          prevSlide();
+        }
+      }
+  }
+});
   
