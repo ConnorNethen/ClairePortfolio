4ed81986a48e8b6a1788f71f93eab1e254fb0ac1
kamith <kvml96@gmail.com>
2024-03-03 18:50:00 -0600

    Co-authored-by: ConnorNethen <ConnorNethen@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
 from flask import Flask, render_template, request, jsonify
 from flask_mail import Mail, Message
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Configure Flask-Mail
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'spamportfolio1@gmail.com'
-app.config['MAIL_PASSWORD'] = 'gvjh wwxj vjny heuu'
+def str_to_bool(s):
+    return s.lower() in ['true', '1', 't', 'y', 'yes']
+
+# Configure Flask-Mail with environment variables and proper type conversion
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))  # Ensure it's an integer
+app.config['MAIL_USE_TLS'] = str_to_bool(os.getenv('MAIL_USE_TLS', 'true'))
+app.config['MAIL_USE_SSL'] = str_to_bool(os.getenv('MAIL_USE_SSL', 'false'))
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 mail = Mail(app)
 
 @app.route('/send_email', methods=['POST'])
 def send_email():
     data = request.json
+
+    if not data or 'name' not in data or 'email' not in data or 'message' not in data:
+        return jsonify({'error':'Missing data'}), 400
+    
     name = data.get('name')
     email = data.get('email')
     message_body = data.get('message')
 
-    msg = Message('Contact Form Submission',
-                  sender='spamportfolio1@gmail.com',
-                  recipients=['ConnorNethen258@gmail.com'])
-    msg.body = f"{name} visited your website and would like to contact you!\nEmail: {email}\nMessage:\n{message_body}"
-    mail.send(msg)
+    if not name or not email or not message_body:
+        return jsonify({'error':'Invalid input'}), 400
 
+    try:
+        msg = Message('Contact Form Submission',
+                    sender= os.getenv('SENDER'),
+                    recipients=[os.getenv('RECIPIENT')])
+        msg.body = f"{name} visited your website and would like to contact you!\nEmail: {email}\nMessage:\n{message_body}"
+        mail.send(msg)
+    except Exception as e:
+        return jsonify({'error': 'Failed to send email'}), 500
+    
     return jsonify({'status': 'Email sent successfully'})
 
 
