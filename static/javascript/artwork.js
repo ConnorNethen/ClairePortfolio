1ed82ea5d0b3f22c49235ec2c1a61b801066e77e
ConnorNethen <ConnorNethen258@gmail.com>
2024-04-03 22:25:43 -0500

    Added page title and favicon. Added fade-in/out animation for artwork section modal

--- static/javascript/artwork.js
+++ static/javascript/artwork.js
@@ -14,19 +14,60 @@ document.addEventListener('DOMContentLoaded', function () {
     var numberText = document.getElementsByClassName("numberText")[0];
     var prevButton = document.querySelector('.prevSlide'); // Use querySelector for single element
     var nextButton = document.querySelector('.nextSlide'); // Use querySelector for single element
+    let isOpen = false;
+    let isTransitioning = false;
 
-
-    // Function to open the modal with a specific image
     function openModal(index) {
-        var image = images[index];
-        modalImg.src = image.src;
-        let [capt, medi] = parseText(image.alt);
-        captionText.textContent = capt;
-        mediumText.textContent = medi;
-        numberText.textContent = (index+1) + ' / ' + totalImages;
-        modal.style.display = "flex";
-        document.body.style.overflow = 'hidden'; // Disable scrolling
-        currentIndex = index; // Update current index
+        if (isTransitioning) return; // If a transition is already in-progress, return
+        if (isOpen) {
+            // Modal is already open, do animation
+            loadAndAnimateImage(index);
+        } else {
+            // Modal has just been opened, no animation
+            isOpen = true;
+            modal.style.display = "flex";
+            document.body.style.overflow = 'hidden'; // Disable scrolling
+            var image = images[index];
+            modalImg.src = image.src;
+            let [capt, medi] = parseText(image.alt);
+            captionText.textContent = capt;
+            mediumText.textContent = medi;
+            numberText.textContent = (index + 1) + ' / ' + totalImages;
+            currentIndex = index; // Update current index
+        }
+    }
+
+    function loadAndAnimateImage(index) {
+        if (isTransitioning) return; // If a transition is already in-progress, return
+        isTransitioning = true; // Indicate that a transition is starting
+
+        // Animate current image out
+        modalImg.classList.add('fade-out');
+    
+        // Wait for the animation to complete
+        setTimeout(() => {
+            // Reset classes and set next image
+            modalImg.className = 'modal-content'; // Reset all classes
+            var image = images[index];
+            modalImg.src = image.src;
+            let [capt, medi] = parseText(image.alt);
+            captionText.textContent = capt;
+            mediumText.textContent = medi;
+            numberText.textContent = `${index + 1} / ${totalImages}`;
+    
+            // Animate next image in
+            modalImg.classList.add('fade-in');
+    
+            // Force a reflow ensuring transitions are applied correctly
+            void modalImg.offsetHeight;
+    
+            // Animate next image in by removing the preparation class
+            setTimeout(() => {
+                modalImg.classList.remove('fade-in');
+                isTransitioning = false; // Indicate that a transition has ended
+            }, 25); // A minimal timeout to ensure class changes are registered
+    
+        }, 500); // This timeout should match the longest CSS transition time
     }
 
     // Function to parse the alt text from the image
@@ -35,15 +76,18 @@ document.addEventListener('DOMContentLoaded', function () {
         return [text.slice(0,index), text.slice(index+1, text.length)];
     }
 
-    // Event listeners for prev and next buttons
+    // Event listener for prev button
     prevButton.addEventListener('click', function(event) {
         event.stopPropagation(); // Prevent modal from closing
+        if (isTransitioning) return; // Prevent action if a transition is in progress
         currentIndex = (currentIndex - 1 + totalImages) % totalImages;
         openModal(currentIndex);
     });
 
+    // Event listener for next button
     nextButton.addEventListener('click', function(event) {
         event.stopPropagation(); // Prevent modal from closing
+        if (isTransitioning) return; // Prevent action if a transition is in progress
         currentIndex = (currentIndex + 1) % totalImages;
         openModal(currentIndex);
     });
